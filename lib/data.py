b4064119b042c4d5d170eaa0b2fc0c8582a7e643
jonnydark <jonny@jonnydark.com>
2015-06-01 23:52:56 +0100

    Almost there but I can't seem to populate the database >__>

--- lib/data.py
+++ lib/data.py
@@ -3,6 +3,7 @@
 
 import urllib
 import os
+import sys
 import gzip
 import sqlite3
 
@@ -29,15 +30,39 @@ def init_database(path):
     conn = sqlite3.connect(os.path.join(path, "verbs.db"))
     cur = conn.cursor()
 
+    # maybe drop table before hand if table exits already
     print("creating tables...")
     cur.execute('''CREATE TABLE verbs (
                     id INTEGER PRIMARY KEY,
-                    kana text,
-                    kanji text,
-                    type text,
-                    english text)''')
+                    kana TEXT,
+                    kanji TEXT,
+                    type TEXT,
+                    english TEXT
+                    jlpt INTEGER)''')
 
     print("populating tables...")
     verbs = [entry for entry in dictionary
-             if "verb" in entry.find('sense').find('pos').text]
-    print("{0} verbs found".format(len(verbs)))
+             if " verb" in entry.find('sense').find('pos').text]
+
+    for idx, verb in enumerate(verbs):
+        kana = verb.find('r_ele')[0].text
+
+        kanji_container = verb.find('k_ele')[0]
+        kanji = ""
+        if kanji_container is not None:
+            kanji = kanji_container.text
+
+        sense = verb.find('sense')
+
+        verb_type = sense.find('pos').text
+        english = ", ".join([gloss.text
+                            for gloss in sense.findall('gloss')])
+        jlpt = 0  # for now
+
+        cur.execute('INSERT INTO verbs VALUES (?,?,?,?,?)',
+                    [kana, kanji, verb_type, english, jlpt])
+
+        if idx % 10 is 0:
+            sys.stdout.write('.')
+
+    print("{0} verbs added".format(idx))
